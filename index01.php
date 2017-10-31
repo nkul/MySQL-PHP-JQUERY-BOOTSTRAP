dd8189a051e977cc05998e1a5d8d5a28cf649de4
nkul <32372523+nkul@users.noreply.github.com>
2017-10-30 18:41:03 -0700

    Add files via upload

---
+++ index01.php
@@ -0,0 +1,31 @@
+<DOCTYPE html>
+<html>
+    <head>
+	    <meta charset="UTF-8">
+		<title>Nkululeko Store</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<script src="main.js"></script>
+	</head>
+<body>
+    <div class="navbar navbar-inverse navbar-fixed-top">
+	   <div class="container-fluid">
+		 <div class="navbar-header">	
+			<a href="#" class="navbar-brand">Nkululeko Store</a>
+	     </div>
+		 <ul class="nav navbar-nav">
+			 <li><a href="#">Home</a></li>
+			 <li><a href="#">Product</a></li>
+			 <li style="width:300px;left:10px;top:10px;"><input type="text" class="form-control" id="search"></li>
+			 <li style="top:10px; left:20px;"><input type="submit" class="btn btn-primary" id="search_btn"></li>
+		</ul>
+		  <ul class="nav navbar-nav navbar-right">
+			 <li><a href="#">Cart</a></li>
+			 <li><a href="#">Sign In</a></li>
+			 <li><a href="#">Sign Up</a></li>
+			 
+	</div>
+    </nav>
+</body>	
+</html>
